92326f23dc075bf1d2cddb18300b6dc82ad3ed46
fhryazr <fahryazaria@gmail.com>
2024-07-02 13:31:24 +0800

    slicing fitur monitoring

--- src/admin/pages/dashboard/Dashboard.jsx
+++ src/admin/pages/dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ import LineChart from "../../component/DashboardMenu/ChartLine";
 const Dashboard = () => {
   return (
     <Layout>
-      <MenuHeader />
+      <MenuHeader title={"Dashboard FDM"}/>
       <div className="flex flex-col items-center justify-center">
         <div className="flex gap-4 w-full">
           <div className="flex flex-col w-[52vw]">
--- src/admin/pages/monitoring/Monitoring.jsx
+++ src/admin/pages/monitoring/Monitoring.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Layout from '../../component/Layout'
 import MenuHeader from '../../component/MenuHeader'
+import EnhancedTable from '../../component/Monitoring/TableMonitoring'
 
 const Monitoring = () => {
   return (
     <Layout>
       <MenuHeader title={"Data Monitoring"}/>
+      <div>
+        <EnhancedTable />
+      </div>
     </Layout>
   )
 }
