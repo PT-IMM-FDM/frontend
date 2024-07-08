64ce1be8a66bcf1d017ff0c8a54b2c3a6c07a1ad
FelixDeanJ <108813645+FelixDeanJ@users.noreply.github.com>
2024-07-08 08:01:20 +0800

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import { Navigate, BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Dashboard from './admin/pages/dashboard'
-import LoginAdmin from './admin/pages/login-admin'
-import LoginUser from './user/pages/login-user/LoginUser'
-import Monitoring from './admin/pages/monitoring'
-import FDM from './user/pages/FDM-user/FDMForm'
+import {
+  Navigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Dashboard from "./admin/pages/dashboard";
+import LoginAdmin from "./admin/pages/login-admin";
+import LoginUser from "./user/pages/login-user/LoginUser";
+import Monitoring from "./admin/pages/monitoring";
+import FDM from "./user/pages/FDM-user/FDMForm";
 
 function App() {
   return (
     <>
       <Router>
-        <main className='min-h-screen'>
+        <main className="min-h-screen">
           <Routes>
-          <Route path="/login" element={<LoginUser/>}/>
-          <Route path="/FDM-form" element ={<FDM/>}/>
-            <Route path="/login-admin" element={<LoginAdmin/>}/>
-            <Route path="/dashboard" element={<Dashboard/>}/>
-            <Route path="/data-monitoring" element={<Monitoring/>}/>
+            <Route path="/FDM-form" element={<FDM />} />
+            <Route path="/login-admin" element={<LoginAdmin />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/data-monitoring" element={<Monitoring />} />
           </Routes>
         </main>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
